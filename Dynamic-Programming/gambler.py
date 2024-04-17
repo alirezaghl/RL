ef4141ed1ce7c1963923bf8ff26f06e4f573881d
Ali Ghalambor <75223461+alirezaghl@users.noreply.github.com>
2024-04-17 15:02:42 -0700

    Update gambler.py

--- Dynamic-Programming/gambler.py
+++ Dynamic-Programming/gambler.py
@@ -1,13 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from cfg import n, nepochs
 
-
-n = 100
 v = np.zeros(n+1) #Creating dummy vector for saving value states
 v[100] = 1 #Based on the text 'The reward is zero on all transitions except those on which the gambler reaches his goal, when it is +1.'
 q = np.zeros((n+1, n+1)) #Creating action-state matrix
 pi_star = np.ones(n+1)   #Creating dummy vector for saving optimal action in each state
-nepochs = 100
 
 def value_iteration(p_head):
   for i in range(nepochs):
